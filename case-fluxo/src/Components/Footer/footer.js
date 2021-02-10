2f787ec261eb01e158d2613ef9605d7e5fe52345
Igor Godinho <igorgt30@gmail.com>
2021-02-09 22:11:38 -0300

    Product page finished with footer

--- case-fluxo/src/Components/Footer/footer.js
+++ case-fluxo/src/Components/Footer/footer.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import './footer.css';
+import {AiOutlineMail,AiOutlinePhone,AiOutlineInstagram,AiOutlineFacebook,AiOutlineWhatsApp} from 'react-icons/ai';
+import {FaTelegramPlane} from 'react-icons/fa';
 export default function Footer(){
   return(
-    <div className='footer-bg'>
+    <footer className='footer-bg'>
       <div className='footer'>
-
-      </div>
-
-    </div>
-  )
+            <div className="footer-left">
+                <p>Rua Paraiso das Garças, 1678 - Humaitá</p>
+                <p><AiOutlineMail className='footer-mail'/> contato@gmail.com</p>
+                <p><AiOutlinePhone/>(21)3292-5400 </p>
+            </div>
+            <div className="footer-center">
+            <p>Project by Fluxo Consultoria</p>
+            </div>
+            <div className="footer-right">
+                <AiOutlineInstagram className="footer-icon"/>
+                <AiOutlineFacebook className="footer-icon"/>
+                <FaTelegramPlane className="footer-icon"/>
+                <AiOutlineWhatsApp className="footer-icon"/>
+            </div>
+          </div>
+        </footer>
+  );
 }
--- case-fluxo/src/Components/Header/header.js
+++ case-fluxo/src/Components/Header/header.js
@@ -4,7 +4,6 @@ import './header.css'
 import { RiPlantLine } from 'react-icons/ri';
 import { IoPersonOutline } from 'react-icons/io5';
 import { AiOutlineShoppingCart } from 'react-icons/ai';
-import { GiMagnifyingGlass } from 'react-icons/gi';
 
 export default function Header(){
   return(
@@ -15,7 +14,6 @@ export default function Header(){
         </div>
         <div className='search'>
           <input id="search" placeholder="Qual produto orgânico você está procurando?"/>
-          
         </div>
         <div className='icons'>
           <ul>
